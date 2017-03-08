efb723e8230c64c00f6894cb724cbce293b8b7e7
topecongiro <seuchida@gmail.com>
2017-03-09 00:50:19 +0900

    Allow command line arguments for setting thread & loop number

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,31 @@
 extern crate libmcs;
 
+use std::str::FromStr;
+use std::env;
 use std::thread;
 use std::sync::Arc;
 use std::sync::mpsc;
 use std::time;
 
+/// A default value for thread number.
 const THREAD_NUM: usize = 4;
+
+/// A default number for number of critical section per thread.
 const LOOP: usize = 1000000;
 
-fn simple_benchmark(use_mcs: bool) -> f64 {
+/// Run a simple benchmark.
+fn simple_benchmark(use_mcs: bool, thread_num: usize, loop_num: usize) -> f64 {
     let (tx, rx) = mpsc::channel();
 
     if use_mcs {
         let data = Arc::new(libmcs::Mutex::new(0));
 
-        for _ in 0..THREAD_NUM {
+        for _ in 0..thread_num {
             let (data, tx) = (data.clone(), tx.clone());
 
             thread::spawn(move || {
                 let start = time::Instant::now();
-                for _ in 0..LOOP {
+                for _ in 0..loop_num {
                     let mut data = data.lock().unwrap();
                     for _ in 0..100 {
                         *data += 1;
@@ -31,12 +37,12 @@ fn simple_benchmark(use_mcs: bool) -> f64 {
     } else {
         let data = Arc::new(std::sync::Mutex::new(0));
 
-        for _ in 0..THREAD_NUM {
+        for _ in 0..thread_num {
             let (data, tx) = (data.clone(), tx.clone());
 
             thread::spawn(move || {
                 let start = time::Instant::now();
-                for _ in 0..LOOP {
+                for _ in 0..loop_num {
                     let mut data = data.lock().unwrap();
                     for _ in 0..100 {
                         *data += 1;
@@ -48,16 +54,33 @@ fn simple_benchmark(use_mcs: bool) -> f64 {
     }
 
     let mut total_time: f64 = 0.0;
-    for i in 0..THREAD_NUM {
+    for i in 0..thread_num {
         let time = 0.000000001 * rx.recv().unwrap() as f64;
         println!("Thread {}: {}", i, time);
         total_time += time;
     }
-    total_time / THREAD_NUM as f64
+    total_time / thread_num as f64
+}
+
+/// A simple parser for command line arguments.
+/// Call this in ascending order with respect to `index` since this uses `nth()`
+/// on `args` internally and consumes previous elements.
+fn parse_args(args: &mut env::Args, index: usize, default: usize) -> usize {
+    match args.nth(index) {
+        Some(ref n) => match usize::from_str(n) {
+            Ok(n) => n,
+            Err(_) => default,
+        },
+        None => default,
+    }
 }
 
 fn main() {
-    println!("The number of thread: {}, the loop: {}", THREAD_NUM, LOOP);
-    println!("Elapsed time for std::sync::Mutex is {}", simple_benchmark(false));
-    println!("Elapsed time for libmcs::Mutex is {}", simple_benchmark(true));
+    let mut args = &mut env::args();
+    let thread_num = parse_args(args, 1, THREAD_NUM);
+    let loop_num = parse_args(args, 2, LOOP);
+
+    println!("The number of thread: {}, the loop: {}", thread_num, loop_num);
+    println!("Elapsed time for std::sync::Mutex is {}", simple_benchmark(false, thread_num, loop_num));
+    println!("Elapsed time for libmcs::Mutex is {}", simple_benchmark(true, thread_num, loop_num));
 }
